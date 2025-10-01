c37d73477d36fdde14674072a3233b4294b3cba2
Camila Silva da Penha <camilaspenha@gmail.com>
2025-10-01 15:06:42 -0300

    css: hero style

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -18,10 +18,18 @@
             <?php endif; ?>
 
             <?php if($args['botao'] && $args['botao']['label']): ?>
-            <div class="button">
-              <a href="<?= $args['botao']['link'] ?>" aria-label="Link para rolar a página até a seção <?= $args['botao']['label'] ?>" class="btn btn_primary">
-                <?= $args['botao']['label'] ?>
-              </a>
+              <div class="hero_btnContainer">
+                <div class="button">
+                <a href="<?= $args['botao']['link'] ?>" aria-label="Link para rolar a página até a seção <?= $args['botao']['label'] ?>" class="btn btn_primary">
+                  <?= $args['botao']['label'] ?>
+                </a>              
+              </div>
+              <div class="button">
+                <a href="<?= get_theme_mod('whatsapp-api') ?>" target="_blank" aria-label="Link para Orçamento via Whatsapp - Abre em nova página" class="btn btn_outline_light">
+                  <i class="bi bi-whatsapp"></i>
+                  <span class="ms-2"> Solicite Orçamento </span>
+                </a>
+              </div>
             </div>
           <?php endif; ?>
         </div>
